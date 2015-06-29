21023a31e67a8754da91d21f08b34f15a88c4e67
GrahamTheCoder <grahamthecoder@gmail.com>
2015-06-29 10:56:31 +0100

    Red: Test money string contained in account string

---
+++ BankingKataTests/TextOutputTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingKata;
+using NUnit.Framework;
+
+namespace BankingKataTests
+{
+    [TestFixture]
+    class TextOutputTests
+    {
+        [Test]
+        public void CurrentBalanceIsPrinted()
+        {
+            var transactionLog = new Ledger();
+            var acc = new Account(transactionLog);
+            var output = new StringWriter();
+            var printer = new AccountPrinter(output);
+            printer.PrintCurrentBalance(acc);
+            Assert.That(output.ToString(), Is.StringContaining(new Money(0m).ToString()));
+        }
+    }
+
+    internal class AccountPrinter
+    {
+        private TextWriter m_StreamWriter;
+
+        public AccountPrinter(TextWriter streamWriter)
+        {
+            this.m_StreamWriter = streamWriter;
+        }
+
+        public void PrintCurrentBalance(Account acc)
+        {
+
+        }
+    }
+}

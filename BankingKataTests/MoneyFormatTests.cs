ce76e8688ce3ff4d433d16bcf236eb237df98e1a
GrahamTheCoder <grahamthecoder@gmail.com>
2015-06-29 11:40:40 +0100

    Passing test for negative monies.

--- BankingKataTests/MoneyFormatTests.cs
+++ BankingKataTests/MoneyFormatTests.cs
@@ -6,19 +6,23 @@ namespace BankingKataTests
     [TestFixture]
     public class MoneyFormatTests
     {
+        private string m_CurrencySymbol = "£";
+
         [Test]
         public void PrintsCurrencySymbol()
         {
-            Assert.That(new Money(0m).ToString(), Is.StringStarting("£"));
+            Assert.That(new Money(0m).ToString(), Is.StringStarting(m_CurrencySymbol));
         }
 
 
         [TestCase(0, "0.00")]
         [TestCase(1234, "1,234.00")]
+        [TestCase(-1234, "-1,234.00")]
         public void ToStringConvertsTheDecimalValueToAString(decimal moneyValue, string expectedMoneyStringWithoutCurrency)
         {
             var actualMoneyString = new Money(moneyValue).ToString();
-            Assert.That(actualMoneyString, Is.StringEnding(expectedMoneyStringWithoutCurrency));
+            var actualMoneyStringWithoutCurrency = actualMoneyString.Replace(m_CurrencySymbol, "");
+            Assert.That(actualMoneyStringWithoutCurrency, Is.EqualTo(expectedMoneyStringWithoutCurrency));
         }
     }
 }
